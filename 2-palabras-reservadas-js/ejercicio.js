0bc98f8c650be566a9c5c0e2830ff7ce59a1e16f
Víctor A <victor.d.araya@gmail.com>
2022-12-15 05:08:20 -0300

    ejercicio modulo 2 completado

---
+++ 2-palabras-reservadas-js/ejercicio.js
@@ -0,0 +1,14 @@
+const aboutMe = [
+  'varayac',
+  33,
+  true,
+  new Date(1990, 2, 13).toLocaleDateString('en-GB'),
+  {
+    title: 'The Dark Tower VI: Song of Susannah',
+    author: 'Stephen King',
+    annio: 2004,
+    URL: 'https://tinyurl.com/the-dark-tower',
+  },
+]
+
+console.log(aboutMe)
